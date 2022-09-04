198bdd13962d3d2867fe5cb200ac24e8e5447ef2
DanielK <Danielkang13@gmail.com>
2022-09-04 09:33:58 -0700

    Created basic route and handlebars to display a simple homepage

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
-const session = require('express-session'); 
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const routes = require('./routes');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const hbs = exphbs.create({});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
 
 app.listen(PORT, (req,res) => {
     console.log('Server Established!');
