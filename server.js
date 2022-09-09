5821ea116b30dfc2ceebfb7d752288403378a20e
DanielK <Danielkang13@gmail.com>
2022-09-08 20:22:33 -0700

    updated calling hompage partials

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const hbs = exphbs.create({});
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(express.static(path.join(__dirname, 'Public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
