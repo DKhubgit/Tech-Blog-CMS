a27ba517eda36e31b54aa8ff313b044829f3c93c
DanielK <Danielkang13@gmail.com>
2022-09-08 19:43:42 -0700

    fixed spelling

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const path = require('path');
-const routes = require('./routes');
+const routes = require('./routes/index');
 require('dotenv').config();
 
 const app = express();
@@ -12,7 +12,7 @@ const hbs = exphbs.create({});
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'Public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
